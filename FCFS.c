6c372cbbcad8b7d9c891fa45b07e7a3e0f602e85
PilgrimV1 <erkh_munkh@yahoo.com>
2026-08-24 18:10:24 +0200

    Added Semaphores and Child ID identification

--- FCFS.c
+++ FCFS.c
@@ -3,30 +3,36 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <semaphore.h>
+#include <time.h>
 
 struct process {
     pid_t real_pid;
-    int arrival_time;
-    int burst_time;
-    int start_time;
-    int completion_time;
+    int arrival_time;   // time when process enters ready queue
+    int burst_time;     // total time the CPU needs to run the process
+    int completion_time;// time when the process finishes its task
+    int turnaround_time;// total time from arrival to completion
+    int waiting_time;   // total time spent waiting in the ready queue
 };
 
 int main (void) {
     int status; // Process Status
+    int n;      // Process Quantity Input from the User
     pid_t child_id;
+    printf("Input number of processes to be generated (5-10): ");
+    scanf("%d", &n);
 
-    child_id = fork ();
+    sem_t sems[n];
+    struct process CP_ID[n]; // not sure what name to call it here
 
-    if (child_id == 0) {
-        printf("Child Process ID: %d\n", getpid());
-    } else { // Parent process waits
-        if (wait(&status) != child_id) {
-            printf("ERROR: Parent process failed to wait!\n"); 
+    for (int i = 0; i < n; i++) {
+        sem_init(&sems[i], 1, 0);
+        child_id = fork();  // Forks creating a child
+        if (child_id == 0) { // Check if the process is a child
+            break;           // The Child is excluded from forking 
         } else {
-            printf("Child Process has been successfully terminated with the status: %d\n", status);
-        }
-    }
-    return 0;
 
+        }
+        CP_ID[i].real_pid = child_id; // Identification for the child id
+    }
+        return 0;
 }
