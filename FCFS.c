59ac8e15487a3175ad4a05de064ad1053e1b4053
PilgrimV1 <erkh_munkh@yahoo.com>
2026-08-25 02:51:17 +0200

    Base FCFS Portion Complete

--- FCFS.c
+++ FCFS.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <semaphore.h>
 #include <time.h>
+#include <fcntl.h>
 
 struct process {
     pid_t real_pid;
@@ -21,32 +22,46 @@ int main (void) {
     printf("Input number of processes to be generated (5-10): ");
     scanf("%d", &n);
 
-    sem_t sems[n];
+    sem_t *start_sems[n];
+    sem_t *end_sems[n];
     struct process CP_ID[n]; // not sure what name to call it here
 
     int i;
     for (i = 0; i < n; i++) {
-        sem_init(&sems[i], 1, 0); // initialize semaphores within loop
+        char name[32];
+        snprintf(name, sizeof(name), "/start_%d", i);
+        start_sems[i] = sem_open(name, O_CREAT, 0644, 0);
+
+        snprintf(name, sizeof(name), "/end_%d", i);
+        end_sems[i] = sem_open(name, O_CREAT, 0644, 0);
+
+
+        /*  ON LINUX:
+        sem_init(&start_sems[i], 1, 0);
+        sem_init(&end_sems[i], 1, 0);
+        */ 
         CP_ID[i].burst_time = (rand() % 5) + 1; // 1-5 seconds
         child_id = fork();  // Forks creating a child
         if (child_id == 0) { // Check if the process is a child
             break;           // The Child is excluded from forking 
         }
         CP_ID[i].real_pid = child_id; // Identification for the child id
+        printf("Process %d (PID: %d) created, burst: %d\n", i, child_id, CP_ID[i].burst_time);
     }
 
     if (child_id == 0) {
-        sem_wait(&sems[i]);
-        printf("Process %d (PID: %d) starting, burst: %d\n", i, getpid(), CP_ID[i].burst_time);
+        sem_wait(start_sems[i]);
         sleep(CP_ID[i].burst_time);
-        printf("Process %d (PID: %d) done\n", i, getpid());
+        printf("Process %d (PID: %d) executing\n", i, getpid());
+        sem_post(end_sems[i]); // to signal the end
         exit(0);
     } else {    // Parent process
-        sem_post(&sems[0]);      // Parent process upholding the other processes
+        sem_post(start_sems[0]);      // Parent process upholding the other processes
         for (int i = 0; i < n; i++) {
-            waitpid(CP_ID[i].real_pid, &status, 0); // Generating the waits for the children
+            sem_wait(end_sems[i]);
+            waitpid(CP_ID[i].real_pid, &status, 0); // Generating the waits for the children, returns 0 while child is still alive 
             if (i < n - 1) {
-                sem_post(&sems[i+1]);
+                sem_post(start_sems[i+1]);
             }
         }
     }
